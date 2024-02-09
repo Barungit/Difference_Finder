335b88d14028b102f68f42584a02110910503c24
Barungit <barunkr0801@gmail.com>
2024-02-09 15:23:36 +0530

    ckecking difference

--- find.js
+++ find.js
@@ -2,7 +2,12 @@ function tell(){
     console.log("Barun");
     var text1 = document.getElementById("text1").value; 
     var text2 = document.getElementsByName("text2")[0].value;
-    document.getElementById("diff").value = text1+text2; 
+    if(text1===text2){
+        document.getElementById("diff").value = "No Diff";     
+    } else {
+        document.getElementById("diff").value = text1+text2; 
+    }
+    
     console.log(text1+text2);
     //console.log(text2);
 }
